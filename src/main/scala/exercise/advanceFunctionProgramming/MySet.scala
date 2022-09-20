4c9c95c90a8f4b8d52f42989b17c0452ee8a1304
harshangp20 <harshangp@tridhyaintuit.com>
2022-09-20 17:55:24 +0530

    Adding more to MySet.scala

--- src/main/scala/exercise/advanceFunctionProgramming/MySet.scala
+++ src/main/scala/exercise/advanceFunctionProgramming/MySet.scala
@@ -21,6 +21,14 @@ trait MySet[A] extends (A => Boolean) {
 
   def forEach(f: A => Unit): Unit
 
+  def -(elem: A): MySet[A]
+
+  def --(anotherSet: MySet[A]): MySet[A] // difference
+
+  def &(anotherSet: MySet[A]): MySet[A] // intersection
+
+  def unary_! : MySet[A]
+
 }
 
 class EmptySet[A] extends MySet[A] {
@@ -38,6 +46,40 @@ class EmptySet[A] extends MySet[A] {
   def filter(predicate: A => Boolean): MySet[A] = this
 
   def forEach(f: A => Unit): Unit = ()
+
+  //  impelmentation
+
+  def -(elem: A): MySet[A] = this
+
+  def --(anotherSet: MySet[A]): MySet[A] = this
+
+  def &(anotherSet: MySet[A]): MySet[A] = this
+
+  def unary_! : MySet[A] = new AllInclusiveSet[A]
+
+}
+
+class AllInclusiveSet[A] extends MySet[A] {
+
+  override def contains(elem: A): Boolean = true
+  override def +(elem: A): MySet[A] = this
+  override def ++(anotherSet: MySet[A]): MySet[A] = this
+
+  override def map[B](f: A => B): MySet[B] = ???
+
+  override def flatMap[B](f: A => MySet[B]): MySet[B] = ???
+
+  override def filter(predicate: A => Boolean): MySet[A] = ???
+
+  override def forEach(f: A => Unit): Unit = ???
+
+  override def -(elem: A): MySet[A] = ???
+
+  override def --(anotherSet: MySet[A]): MySet[A] = ???
+
+  override def &(anotherSet: MySet[A]): MySet[A] = ???
+
+  override def unary_! : MySet[A] = ???
 
 }
 
@@ -72,6 +114,19 @@ class NonEmptySet[A](head: A, tail: MySet[A]) extends MySet[A] {
     tail forEach f
 
   }
+
+  def -(elem: A): MySet[A] =
+    if (head == elem) tail
+    else tail - elem + head
+
+  def --(anotherSet: MySet[A]): MySet[A] = filter(!anotherSet)
+
+  def &(anotherSet: MySet[A]): MySet[A] = filter(anotherSet)
+
+//  Will implement this method soon
+
+  def unary_! : MySet[A] = ???
+
 }
 
 object MySet {
@@ -90,9 +145,9 @@ object MySet {
 
 object MySetVariables extends App {
 
-  val s = MySet(1,2,3,4)
+  val s = MySet(1, 2, 3, 4)
 
-  s + 4 + 5  ++ MySet(6,7,8) + 9 flatMap (x => MySet(x, 12 * x)) forEach println
+  s + 4 + 5 ++ MySet(6, 7, 8) + 9 flatMap (x => MySet(x, 12 * x)) forEach println
 
 
 }
